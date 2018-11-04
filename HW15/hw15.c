fcb824bfbfe54677b81ec93cd4d254621cd36608
Jacob Willis <willisjc@purdue.edu>
2018-11-04 03:51:27 -0500

    HW15 completion

--- HW15/hw15.c
+++ HW15/hw15.c
@@ -5,45 +5,82 @@
 
 #ifdef LINKEDLIST
 // Do not modify this function.
-void LinkedListPrint(Node * head)
+void LinkedListPrint(Node *head)
 {
 
 	Node *temp = head;
 
-	while(temp!=NULL)
+	while (temp != NULL)
 	{
-		if(temp->next!=NULL)
+		if (temp->next != NULL)
 		{
-			printf("%d,",temp->value);
+			printf("%d,", temp->value);
 		}
 		else
 		{
-			printf("%d\n",temp->value);
+			printf("%d\n", temp->value);
 		}
-		temp=temp->next;
+		temp = temp->next;
 	}
 }
 #endif
 
-
 /********** Do Not modify the file above this line, you can modify below ***********/
 #ifdef TEST_CREATENODE
 
-Node * CreateNode(int val)
+Node *CreateNode(int val)
 {
 	/*
 	Create a new Node with `value` set to `val`. Set `next` to NULL.
 	*/
+	Node *node = malloc(sizeof(Node));
+	node->value = val;
+	node->next = NULL;
+	return node;
 }
 #endif
 
 #ifdef TEST_LINKEDLISTCREATE
 
-void LinkedListCreate(Node ** head, char* name)
+void LinkedListCreate(Node **head, char *name)
 {
 	/*
 	Create a linked list from the input file referenced by `name`.
 	*/
+	FILE *fptr = fopen(name, "r");
+	int x = 0;
+	int count = 0;
+	while (fscanf(fptr, "%d", &x) != EOF)
+	{
+		count++;
+	}
+	// allocate memory to store the numbers
+	int *arr = malloc(sizeof(int) * count);
+	if (!arr)
+	{
+		fprintf(stderr, "failed to malloc for int array");
+		return;
+	}
+	// use fscanf to read the file for its contents.
+	fseek(fptr, 0, SEEK_SET);
+	int i;
+	for (i = 0; i < count; i++)
+	{
+		fscanf(fptr, "%d", &arr[i]);
+	}
+	fclose(fptr);
+	if (count < 0)
+	{
+		return;
+	}
+	Node *node = CreateNode(arr[0]);
+	*head = node;
+	for (i = 1; i < count; i++)
+	{
+		node->next = CreateNode(arr[i]);
+		node = node->next;
+	}
+	free(arr);
 }
 #endif
 #ifdef TEST_REMOVED
@@ -59,5 +96,30 @@ void RemoveDuplicate(Node *headRef)
 	To delete a node: we will map the next Node of the previous Node to the Node after the current Node.
 	Print the linked list after all repetitions have been removed. Use the function given to you for priniting.
 	*/
+	Node *node = headRef;
+	Node *check = NULL;
+	int rel;
+	while (node->next)
+	{
+		check = headRef;
+		rel = 1;
+		while (check != (node->next))
+		{
+			if (check->value == node->next->value)
+			{
+				Node *temp = node->next;
+				node->next = node->next->next;
+				free(temp);
+				rel = 0;
+			}
+			check = rel?check->next:node->next;
+		}
+		if (rel)
+		{
+			node = node->next;
+		}
+	}
+
+	LinkedListPrint(headRef);
 }
 #endif
--- HW15/main.c
+++ HW15/main.c
@@ -13,11 +13,22 @@ int main(int argc, char **argv)
 {
 	// input argv[1] contains the name of the input file.
 	// Check for the right number of input arguments. 
+	if (argc != 2) {
+		return EXIT_FAILURE;
+	}
 
 	//Initialize a linked list Node and use the file name from input arguments to create a linked list
+	Node * node = NULL;
+	Node ** head = &node;
+	LinkedListCreate(head, argv[1]);
 	//Remove all duplicates in the linked list created above.
-
+	RemoveDuplicate(*head);
 	// release memory
+	do {
+    Node * charon = node;
+    node = node->next;
+    free(charon);
+  } while (node);
 }
 
 #endif
